45a29510a618b99542ecac84d63ab3d1b6013474
sammie <adenijioluseguntope@gmail.com>
2023-08-06 22:17:12 +0100

    Step two

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 extension CompactMap<T> on Iterable<T?> {
@@ -32,32 +33,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const url = "https://bit.ly/47o4yXK";
+
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState("");
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-        });
-        return null;
-      },
-      [controller],
-    );
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+        .load(url)
+        .then((data) => data.buffer.asUint8List())
+        .then((data) => Image.memory(data)));
+
+    final snapshot = useFuture(future);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
       body: Column(
-        children: [
-          TextField(controller: controller),
-          const SizedBox(height: 10),
-          Text("You typed: ${text.value}"),
-        ],
+        children: [snapshot.data].compactMap().toList(),
       ),
     );
   }
