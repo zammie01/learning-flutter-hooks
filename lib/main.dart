43a68cd2e83779b59aa9dbb1eebde683e377da57
sammie <adenijioluseguntope@gmail.com>
2023-08-06 21:26:39 +0100

    Step one

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
-import 'dart:async';
-import 'dart:ffi';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 extension CompactMap<T> on Iterable<T?> {
@@ -37,41 +32,33 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const url = "https://bit.ly/47o4yXK";
-
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    late final StreamController<double> controller;
-    controller = useStreamController<double>(onListen: () {
-      controller.sink.add(0.0);
-    });
+    final controller = useTextEditingController();
+    final text = useState("");
+    useEffect(
+      () {
+        controller.addListener(() {
+          text.value = controller.text;
+        });
+        return null;
+      },
+      [controller],
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: StreamBuilder<double>(
-          stream: controller.stream,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const CircularProgressIndicator();
-            } else {
-              final rotation = snapshot.data ?? 0.0;
-              return GestureDetector(
-                onTap: () {
-                  controller.sink.add(rotation + 10.0);
-                },
-                child: RotationTransition(
-                  turns: AlwaysStoppedAnimation(rotation / 360.0),
-                  child: Center(
-                    child: Image.network(url),
-                  ),
-                ),
-              );
-            }
-          }),
+      body: Column(
+        children: [
+          TextField(controller: controller),
+          const SizedBox(height: 10),
+          Text("You typed: ${text.value}"),
+        ],
+      ),
     );
   }
 }
